c4053deb518550548d76fdca472b361da9a0aede
Jason Coleman <jcoleman@ufl.edu>
2016-09-24 22:31:39 -0400

    Fixed an error in process* and modified EWMA.m (JEC)

--- EWMA.m
+++ EWMA.m
@@ -3,13 +3,14 @@ function smoothed_z = EWMA(z,L)
 % Computes the exponentially weighted moving average (with memory L) of
 % input data z
 %
+% 9/24/16 - Removed 'i' loop since we are only using a vector/1D array for z
 
 lambda = 1-2/(L+1);
 
 smoothed_z = zeros(size(z));
-for i = 1:size(z,1)
-    smoothed_z(i,1) = z(i,1);
-    for j = 2:size(z,2)
-        smoothed_z(i,j) = lambda * smoothed_z(i,j-1) + (1-lambda) * z(i,j);
-    end
+
+smoothed_z = z;
+
+for j = 2:size(z,2)
+    smoothed_z(j) = lambda * smoothed_z(j-1) + (1-lambda) * z(j);
 end
